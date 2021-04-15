0462d6f7fec29eacb28dd1b75ef4a33d1fe6c2df
Andrew Morris <voltrevo@gmail.com>
2021-04-15 21:13:22 +1000

    Attempt to make Wallet work in deno

--- src/app/deps.ts
+++ src/app/deps.ts
@@ -11,16 +11,19 @@ export type {
 } from "https://deno.land/x/oak/mod.ts";
 
 // Database dependencies
-export { Client } from "https://deno.land/x/postgres/mod.ts"
-export {
-    QueryClient,
-    DataType,
-    Constraint,
-    CreateTableMode,
-} from "https://deno.land/x/postquery/mod.ts"
+// export { Client } from "https://deno.land/x/postgres/mod.ts"
+// export {
+//     QueryClient,
+//     DataType,
+//     Constraint,
+//     CreateTableMode,
+// } from "https://deno.land/x/postquery/mod.ts"
 
-export type {
-    TableOptions
-} from "https://deno.land/x/postquery/mod.ts"
+// export type {
+//     TableOptions
+// } from "https://deno.land/x/postquery/mod.ts"
 
+import type { Wallet as WalletType } from "https://cdn.skypack.dev/-/@ethersproject/wallet@v5.1.0-mpoCY8R8syeSEFVDZmv5/dist=es2020,mode=types/lib/index.d.ts";
 
+import ethers from "https://dev.jspm.io/ethers@5.1.0";
+export const Wallet = (ethers as any).Wallet as { new(): WalletType };
---
+++ test.ts
@@ -0,0 +1,3 @@
+import { Wallet } from "./src/app/deps.ts";
+
+console.log(Wallet);
